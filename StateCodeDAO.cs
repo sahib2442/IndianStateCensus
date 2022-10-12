f4f01312f47c41c2a3ef159101c6af44d85080fb
sahib2442 <kadersahib1234@gmail.com>
2022-10-12 15:10:28 +0530

    [sahib] Add: Indain state code

---
+++ StateCodeDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndainStateAnalyzer
+{
+    public class StateCodeDAO
+    {
+        public class StateCodeAnalyser
+        {
+            StateCensusAnalyzer analyzer = new StateCensusAnalyzer();
+            StateCodeAnalyser analyser = new StateCodeAnalyser();
+        }
+    }
+}
